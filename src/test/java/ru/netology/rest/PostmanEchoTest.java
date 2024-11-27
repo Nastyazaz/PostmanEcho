9e2f2a47b171f6e68344d4ac2fbfeb298679cac1
Nastyazaz <dynaeva.nastya96@gmail.com>
2024-11-28 01:26:06 +0300

    Update PostmanEchoTest.java

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(200)
+                .statusCode(300)
                 .body("data", equalTo("some data"))
         ;
     }
